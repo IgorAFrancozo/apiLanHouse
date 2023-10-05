68acb8091488057e76b3e9b93b32207ab0a4c1c2
unknown <igorfrancozo9@gmail.com>
2023-10-05 02:38:14 -0300

    Controlador inscrição com regras de negócio

--- src/main/java/br/com/digix/api/controller/InscricaoController.java
+++ src/main/java/br/com/digix/api/controller/InscricaoController.java
@@ -1,10 +1,12 @@
 package br.com.digix.api.controller;
 
 import br.com.digix.api.dominio.inscricao.DadosAgendamentoInscricao;
+import br.com.digix.api.dominio.inscricao.DadosCancelamentoInscricao;
 import br.com.digix.api.dominio.inscricao.DadosDetalhesInscricao;
 import br.com.digix.api.dominio.inscricao.InscricaoPartidaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +20,13 @@ public class InscricaoController {
 
     @PostMapping("/inscrever")
     public ResponseEntity<DadosDetalhesInscricao> inscreverParaPartida(@RequestBody @Valid DadosAgendamentoInscricao dados) {
-        try {
-            DadosDetalhesInscricao inscricao = inscricaoPartidaService.inscrever(dados);
-            return ResponseEntity.status(HttpStatus.CREATED).body(inscricao);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DadosDetalhesInscricao(e.getMessage()));
-        }
+        var inscricao = inscricaoPartidaService.inscrever(dados);
+        return ResponseEntity.status(HttpStatus.CREATED).body(inscricao);
     }
 
     @DeleteMapping("/cancelar/{inscricaoId}")
-    public ResponseEntity<Void> cancelarInscricao(@PathVariable Long inscricaoId) {
-        inscricaoPartidaService.cancelarInscricao(inscricaoId);
+    public ResponseEntity<Void> cancelarInscricao(@PathVariable @Valid DadosCancelamentoInscricao dadosCancelamentoInscricao) {
+        inscricaoPartidaService.cancelarInscricao(dadosCancelamentoInscricao.idInscricao(), dadosCancelamentoInscricao.motivo());
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/br/com/digix/api/dominio/inscricao/InscricaoPartidaService.java
+++ src/main/java/br/com/digix/api/dominio/inscricao/InscricaoPartidaService.java
@@ -3,12 +3,13 @@ package br.com.digix.api.dominio.inscricao;
 import br.com.digix.api.dominio.ValidacaoException;
 import br.com.digix.api.dominio.inscricao.validacoes.ValidadorInscricao;
 import br.com.digix.api.dominio.jogador.JogadorRepository;
-import br.com.digix.api.dominio.jogo.JogoRepository;
 import br.com.digix.api.dominio.partida.PartidaRepository;
+import br.com.digix.api.dominio.partida.StatusPartida;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -33,5 +34,30 @@ public class InscricaoPartidaService {
         inscricaoRepository.save(inscricao);
 
         return new DadosDetalhesInscricao(inscricao);
+    }
+
+    public List<DadosDetalhesInscricao> listarInscricoes() {
+        List<Inscricao> inscricoes = inscricaoRepository.findAll();
+        List<DadosDetalhesInscricao> detalhesInscricoes = new ArrayList<>();
+
+        for (Inscricao inscricao : inscricoes) {
+            detalhesInscricoes.add(new DadosDetalhesInscricao(inscricao));
+        }
+
+        return detalhesInscricoes;
+    }
+
+    public DadosCancelamentoInscricao cancelarInscricao(Long inscricaoId, MotivoCancelamento motivo) {
+        var inscricao = inscricaoRepository.findById(inscricaoId)
+                .orElseThrow(() -> new ValidacaoException("Inscrição não encontrada"));
+
+        if (inscricao.getPartida().getStatus() != StatusPartida.PROGRAMADA) {
+            throw new ValidacaoException("Não é possível cancelar uma inscrição em uma partida já iniciada");
+        }
+
+        inscricao.cancelar(motivo);
+        inscricaoRepository.save(inscricao);
+
+        return new DadosCancelamentoInscricao(inscricao.getId(), motivo);
     }
 }
